4f75d8e8bf8293edf6541d4461137ce4277f9f20
NonthawatNK <Nonthawt20@gmail.com>
2021-01-08 16:32:18 +0700

    yes yes

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+int main(){
+    double sum ,sum2 ,mean,SD;
+    double count = 0;
+    ifstream score;
+    string text;
+    score.open("score.txt");
+    while (getline(score,text))
+    {
+        sum += stod(text);
+        sum2 += pow(stod(text),2);
+        count++;
+    }
+    
+    cout << "Number of data = "<<count<<endl;
+    cout << setprecision(3);
+    mean = sum / count;
+    cout << "Mean = "<<mean<<endl;
+    SD = sqrt(((1/count)*sum2)-pow(mean,2));
+    cout << "Standard deviation = "<<SD;
+}
